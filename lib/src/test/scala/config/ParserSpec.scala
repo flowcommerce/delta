4379ff356664525e27eb5285c42b672711cb47f5
Michael Bryzek <mbryzek@alum.mit.edu>
2016-10-20 14:20:56 -0400

    Update example

--- lib/src/test/scala/config/ParserSpec.scala
+++ lib/src/test/scala/config/ParserSpec.scala
@@ -73,7 +73,7 @@ class ParserSpec extends Specification {
             initialNumberInstances = 10,
             portContainer = 7050,
             portHost = 8000,
-            stages = Seq(BuildStage.SetDesiredState, BuildStage.SyncDockerImage, BuildStage.BuildDockerImage),
+            stages = BuildStage.all.filter { _ != BuildStage.Scale },
             dependencies = Seq("api")
           )
         )
