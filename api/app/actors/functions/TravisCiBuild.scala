a859b25fd68eee5dcf89d7578c55006b3acf632b
Phong Nguyen <pnguyen@hydrasoft.io>
2017-07-10 21:16:02 -0400

    Use TravisCi API to check build status

--- api/app/actors/functions/TravisCiBuild.scala
+++ api/app/actors/functions/TravisCiBuild.scala
@@ -1,10 +1,9 @@
 package io.flow.delta.actors.functions
 
-import db.EventsDao
 import io.flow.delta.actors.BuildEventLog
 import io.flow.delta.config.v0.models.{Build => BuildConfig}
 import io.flow.delta.lib.BuildNames
-import io.flow.delta.v0.models._
+import io.flow.delta.v0.models.{Organization, Project, Build}
 import io.flow.play.util.Config
 import io.flow.travis.ci.v0.Client
 import io.flow.travis.ci.v0.models._
@@ -31,40 +30,68 @@ case class TravisCiBuild(
 
   def buildDockerImage() {
     val dockerImageName = BuildNames.dockerImageName(org.docker, build)
-    val msg = s"Triggered docker build for ${dockerImageName}:${version}"
-    
-    EventsDao.findAll(
-      projectId = Some(project.id),
-      `type` = Some(EventType.Change),
-      summaryKeywords = Some(msg),
-      limit = 1        
-    ).headOption match {
 
-      case None => {
-        client.requests.post(
-          repositorySlug = travisRepositorySlug(),
-          requestPostForm = createRequestPostForm(),
-          requestHeaders = createRequestHeaders()
-        ).map { request =>
-          log.changed(msg)
-        }.recover {
-          case io.flow.docker.registry.v0.errors.UnitResponse(code) => {
-            code match {
-              case _ => {
-                log.error(s"Travis CI returned HTTP $code when triggering build [${dockerImageName}:${version}]")
-              }
+    client.requests.get(
+        repositorySlug = travisRepositorySlug(),
+        limit = Option(5),
+        requestHeaders = createRequestHeaders()
+    ).map { requestGetResponse =>
+      
+      val requests = requestGetResponse.requests
+        .filter(_.eventType == EventType.Api)
+        .filter(_.branchName.name.getOrElse("") == version)
+        .filter(_.commit.message.getOrElse("").contains(dockerImageName))
+
+      requests match {
+        case Seq() => {
+          // no matching builds, submit new build
+          postBuildRequest()
+        }
+        case Seq(_*) => {
+          requests.foreach { request =>
+            request.builds.foreach { build =>
+              log.changed(s"Travis CI build [${dockerImageName}:${version}], number: ${build.number}, state: ${build.state}")
             }
-          }
-          case err => {
-            err.printStackTrace(System.err)
-            log.error(s"Error triggering Travis CI build [${dockerImageName}:${version}]: $err")
           }
         }
       }
+      
+    }.recover {
+      case io.flow.docker.registry.v0.errors.UnitResponse(code) => {
+        code match {
+          case _ => {
+            log.error(s"Travis CI returned HTTP $code when fetching requests [${dockerImageName}:${version}]")
+          }
+        }
+      }
+      case err => {
+        err.printStackTrace(System.err)
+        log.error(s"Error fetching Travis CI requests [${dockerImageName}:${version}]: $err")
+      }
+    }
+  }
+  
+  private def postBuildRequest() {
+    val dockerImageName = BuildNames.dockerImageName(org.docker, build)
 
-      case Some(_) => {
-        log.checkpoint(s"Waiting for triggered build [${dockerImageName}:${version}]")
-      }     
+    client.requests.post(
+      repositorySlug = travisRepositorySlug(),
+      requestPostForm = createRequestPostForm(),
+      requestHeaders = createRequestHeaders()
+    ).map { request =>
+      log.changed(s"Triggered docker build for ${dockerImageName}:${version}")
+    }.recover {
+      case io.flow.docker.registry.v0.errors.UnitResponse(code) => {
+        code match {
+          case _ => {
+            log.error(s"Travis CI returned HTTP $code when triggering build [${dockerImageName}:${version}]")
+          }
+        }
+      }
+      case err => {
+        err.printStackTrace(System.err)
+        log.error(s"Error triggering Travis CI build [${dockerImageName}:${version}]: $err")
+      }
     }
   }
 
