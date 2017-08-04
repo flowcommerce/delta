28dfd3880e50b891e37a5aaf3a107604980f284c
Phong Nguyen <pnguyen@hydrasoft.io>
2017-08-04 16:50:23 -0400

    Catch TimeoutExceptions

--- api/app/actors/functions/TravisCiBuild.scala
+++ api/app/actors/functions/TravisCiBuild.scala
@@ -12,6 +12,7 @@ import io.flow.travis.ci.v0.models._
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Await
 import scala.concurrent.duration._
+import java.util.concurrent.TimeoutException
 
 case class TravisCiBuild(
     version: String,
@@ -37,81 +38,92 @@ case class TravisCiBuild(
 
     BuildLockUtil().withLock(build.id) ({
 
-      val response = client.requests.get(
-          repositorySlug = travisRepositorySlug(),
-          limit = Some(20)
-      )
-      Await.result(response, 5.seconds)
+      try {
+        val response = client.requests.get(
+            repositorySlug = travisRepositorySlug(),
+            limit = Some(20)
+        )
+        Await.result(response, 5.seconds)
 
-      response.map { requestGetResponse =>
+        response.map { requestGetResponse =>
 
-        val requests = requestGetResponse.requests
-          .filter(_.eventType == EventType.Api)
-          .filter(_.branchName.name.getOrElse("") == version)
-          .filter(_.commit.message.getOrElse("").contains(dockerImageName))
+          val requests = requestGetResponse.requests
+            .filter(_.eventType == EventType.Api)
+            .filter(_.branchName.name.getOrElse("") == version)
+            .filter(_.commit.message.getOrElse("").contains(dockerImageName))
 
-        requests match {
-          case Nil => {
-            // No matching builds from Travis. Check the Event log to see
-            // if we tried to submit a build, otherwise submit a new build.
-            EventsDao.findAll(
-              projectId = Some(project.id),
-              `type` = Some(DeltaEventType.Change),
-              summaryKeywords = Some(travisChangedMessage(dockerImageName, version)),
-              limit = 1
-            ).headOption match {
-              case None => {
-                postBuildRequest()
+          requests match {
+            case Nil => {
+              // No matching builds from Travis. Check the Event log to see
+              // if we tried to submit a build, otherwise submit a new build.
+              EventsDao.findAll(
+                projectId = Some(project.id),
+                `type` = Some(DeltaEventType.Change),
+                summaryKeywords = Some(travisChangedMessage(dockerImageName, version)),
+                limit = 1
+              ).headOption match {
+                case None => {
+                  postBuildRequest()
+                }
+                case Some(_) => {
+                  log.checkpoint(s"Waiting for triggered build [${dockerImageName}:${version}]")
+                }
               }
-              case Some(_) => {
-                log.checkpoint(s"Waiting for triggered build [${dockerImageName}:${version}]")
+            }
+            case requests => {
+              requests.foreach { request =>
+                request.builds.foreach { build =>
+                  log.checkpoint(s"Travis CI build [${dockerImageName}:${version}], number: ${build.number}, state: ${build.state}")
+                }
               }
             }
           }
-          case requests => {
-            requests.foreach { request =>
-              request.builds.foreach { build =>
-                log.checkpoint(s"Travis CI build [${dockerImageName}:${version}], number: ${build.number}, state: ${build.state}")
-              }
-            }
+
+        }.recover {
+          case io.flow.docker.registry.v0.errors.UnitResponse(code) => {
+            log.error(s"Travis CI returned HTTP $code when fetching requests [${dockerImageName}:${version}]")
+          }
+          case err => {
+            err.printStackTrace(System.err)
+            log.error(s"Error fetching Travis CI requests [${dockerImageName}:${version}]: $err")
           }
         }
-
-      }.recover {
-        case io.flow.docker.registry.v0.errors.UnitResponse(code) => {
-          log.error(s"Travis CI returned HTTP $code when fetching requests [${dockerImageName}:${version}]")
-        }
-        case err => {
-          err.printStackTrace(System.err)
-          log.error(s"Error fetching Travis CI requests [${dockerImageName}:${version}]: $err")
+      } catch {
+        case e: TimeoutException => {
+          log.error(s"Timeout expired fetching Travis CI requests [${dockerImageName}:${version}]")
         }
       }
-
     })
   }
 
   private def postBuildRequest() {
     val dockerImageName = BuildNames.dockerImageName(org.docker, build)
 
-    val response = client.requests.post(
-      repositorySlug = travisRepositorySlug(),
-      requestPostForm = createRequestPostForm()
-    )
-    Await.result(response, 5.seconds)
+    try {
+      val response = client.requests.post(
+        repositorySlug = travisRepositorySlug(),
+        requestPostForm = createRequestPostForm()
+      )
+      Await.result(response, 5.seconds)
 
-    response.map { request =>
-      log.changed(travisChangedMessage(dockerImageName, version))
-    }.recover {
-      case io.flow.docker.registry.v0.errors.UnitResponse(code) => {
-        code match {
-          case _ => {
-            log.error(s"Travis CI returned HTTP $code when triggering build [${dockerImageName}:${version}]")
+      response.map { request =>
+        log.changed(travisChangedMessage(dockerImageName, version))
+      }.recover {
+        case io.flow.docker.registry.v0.errors.UnitResponse(code) => {
+          code match {
+            case _ => {
+              log.error(s"Travis CI returned HTTP $code when triggering build [${dockerImageName}:${version}]")
+            }
           }
         }
+        case err => {
+          err.printStackTrace(System.err)
+          log.error(s"Error triggering Travis CI build [${dockerImageName}:${version}]: $err")
+        }
       }
-      case err => {
-        err.printStackTrace(System.err)
-        log.error(s"Error triggering Travis CI build [${dockerImageName}:${version}]: $err")
+    } catch {
+      case e: TimeoutException => {
+        log.error(s"Timeout expired triggering Travis CI build [${dockerImageName}:${version}]")
       }
     }
   }
