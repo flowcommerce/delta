982843c00e86056c4027316140e29885cdb5a5b8
Ben Iofel <ben@flow.io>
2020-01-09 18:29:45 -0500

    Regen travis client (#465)
    * Regen travis client
    
    * rm id

--- generated/app/FlowTravisCiV0Client.scala
+++ generated/app/FlowTravisCiV0Client.scala
@@ -178,7 +178,6 @@ package io.flow.travis.ci.v0.models {
   )
 
   final case class RequestRepositoryData(
-    id: Long,
     name: String,
     ownerName: _root_.scala.Option[String] = None,
     slug: _root_.scala.Option[String] = None
@@ -1111,16 +1110,14 @@ package io.flow.travis.ci.v0.models {
 
     implicit def jsonReadsTravisCiRequestRepositoryData: play.api.libs.json.Reads[RequestRepositoryData] = {
       for {
-        id <- (__ \ "id").read[Long]
         name <- (__ \ "name").read[String]
         ownerName <- (__ \ "owner_name").readNullable[String]
         slug <- (__ \ "slug").readNullable[String]
-      } yield RequestRepositoryData(id, name, ownerName, slug)
+      } yield RequestRepositoryData(name, ownerName, slug)
     }
 
     def jsObjectRequestRepositoryData(obj: io.flow.travis.ci.v0.models.RequestRepositoryData): play.api.libs.json.JsObject = {
       play.api.libs.json.Json.obj(
-        "id" -> play.api.libs.json.JsNumber(obj.id),
         "name" -> play.api.libs.json.JsString(obj.name)
       ) ++ (obj.ownerName match {
         case None => play.api.libs.json.Json.obj()
@@ -1725,7 +1722,7 @@ package io.flow.travis.ci.v0 {
       f(play.api.libs.json.Json.parse(r.body)) match {
         case play.api.libs.json.JsSuccess(x, _) => x
         case play.api.libs.json.JsError(errors) => {
-          throw io.flow.travis.ci.v0.errors.FailedRequest(r.status, s"Invalid json[${r.body}] for class[" + className + "]: " + errors.mkString(" "))
+          throw io.flow.travis.ci.v0.errors.FailedRequest(r.status, s"Invalid json for class[" + className + "]: " + errors.mkString(" "))
         }
       }
     }
